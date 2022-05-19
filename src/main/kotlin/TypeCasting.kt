991115276c3d2dadd75bb1a46172157c90c140cf
tashafmukhtar <tashafmukhtar@gmail.com>
2022-05-19 23:07:26 +0400

    readme.md update

---
+++ src/main/kotlin/TypeCasting.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+//    var myFavouriteNo = "6"
+//
+//    var myNo = 7
+//
+//    var finalNO = myFavouriteNo.toInt() + 7
+//
+//    println(finalNO)
+
+
+    var myLongNo = 98139938L
+
+    var myShortNo : Short = myLongNo.toShort()
+    println(myShortNo)
+
+
+
+}
